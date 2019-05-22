53d8856159ea5c9046ac2da006d44a067ad11722
Evgeni Zharkov <zharkov.ev.u@yandex.ru>
2019-05-22 22:26:21 +0300

    Добавлена обработка частичного чтения

--- src/equals.ts
+++ src/equals.ts
@@ -27,27 +27,45 @@ export async function isFileEquals(a: string, b: string): Promise<boolean> {
   if (statA.size !== statB.size) return false;
 
   return new Promise(resolve => {
-    let emitCompare = false;
-    const compareBuffer = Buffer.allocUnsafe(bufSize);
+    let emitCompare = false, cmpBufSize = 0;
+    const cmpBuf = Buffer.allocUnsafe(bufSize);
     const streamA = fs.createReadStream(a, { highWaterMark: bufSize });
     const streamB = fs.createReadStream(b, { highWaterMark: bufSize });
 
     const handleData = (me: fs.ReadStream, compare: fs.ReadStream) => (chunk: Buffer) => {
       me.pause();
       if (emitCompare) {
-        if (chunk.compare(compareBuffer, 0, chunk.byteLength) !== 0) {
+        const cmpSize = Math.min(chunk.byteLength, cmpBufSize);
+        if (chunk.compare(cmpBuf, 0, cmpSize, 0, cmpSize) !== 0) {
           compare.destroy();
           me.destroy();
           return resolve(false);
         }
+
+        if (chunk.byteLength > cmpBufSize) {
+          cmpBuf.fill(chunk.slice(cmpBufSize));
+          cmpBufSize = chunk.byteLength - cmpBufSize;
+          return compare.resume();
+        } else if (chunk.byteLength < cmpBufSize) {
+          cmpBuf.fill(cmpBuf.slice(chunk.byteLength));
+          cmpBufSize = cmpBufSize - chunk.byteLength;
+          return me.resume();
+        } else {
+          emitCompare = false;
+        }
       } else {
-        compareBuffer.fill(chunk);
+        cmpBufSize = chunk.byteLength;
+        cmpBuf.fill(chunk);
+        emitCompare = true;
       }
-      emitCompare = !emitCompare;
-      compare.resume();
+      return compare.resume();
     };
 
-    const handleEnd = () => resolve(true);
+    const handleEnd = () => {
+      streamA.close();
+      streamB.close();
+      resolve(true);
+    };
 
     streamA.on("data", handleData(streamA, streamB)).on("end", handleEnd);
     streamB.on("data", handleData(streamB, streamA)).on("end", handleEnd);
