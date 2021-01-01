b041c39bf83a5a9d25ebc0c5b06b998575829b49
Evgenii Zharkov <zharkov.ev.u@yandex.ru>
2021-01-01 08:16:38 +0300

    Update dependencies

--- test/equals.spec.ts
+++ test/equals.spec.ts
@@ -3,7 +3,7 @@
 import assert from 'assert';
 import 'mocha';
 import path from 'path';
-import isEqual from '../dist/index';
+import isEqual from '../index';
 
 const notExist = path.join(__dirname, 'resource/not-found.jpg');
 const jsLogo = path.join(__dirname, 'resource/js-logo.jpg');
